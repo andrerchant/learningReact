0e22167780d63a96f66aca3504760ae725eb4bcd
Andre <andremerchant@gmail.com>
2019-10-24 22:25:00 -0500

    clase 12

--- src/components/badge.js
+++ src/components/badge.js
@@ -4,8 +4,16 @@ import "./styles/Badge.css"
 import logotipoConf from '../images/badge-header.svg';
 
 class Badge extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            prueba : { cosa : true},
+        }
+    }
     render(){
-        const fotoAndy = "https://instagram.fmex7-1.fna.fbcdn.net/vp/1119985ce3409471f3631c73d30cdb0d/5E64C107/t51.2885-19/s150x150/49683380_796737684024181_8687158980700536832_n.jpg?_nc_ht=instagram.fmex7-1.fna.fbcdn.net";
+        
+        
+
         return (
             <div className="Badge">
                 <div className="Badge__header">
@@ -13,13 +21,15 @@ class Badge extends React.Component{
                 </div>
 
                 <article className="Badge__section-name">
-                    <img className="Badge__avatar" src={fotoAndy} alt="Avatar"/>
-                    <h1> André <br /> Merchant </h1>
+                    <img className="Badge__avatar" src={this.props.avatar} alt="Avatar"/>
+                    <h1> {this.props.firstName} <br /> {this.props.lastName} </h1>
                 </article>
 
                 <div className="Badge__section-info">
-                    <h3>Frontend Engineer</h3>
-                    <a href="https://twitter.com/andrerchant" >@andrerchant</a>
+                    <h3>{this.props.jobTitle}</h3>
+                    <small>
+                        @<a href="https://twitter.com/andrerchant"> {this.props.twitter} </a>
+                    </small>
                 </div>
 
                 <footer className="Badge__footer">
--- src/index.js
+++ src/index.js
@@ -9,7 +9,18 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './global.css';
 import Badge from './components/badge';
 
-ReactDOM.render(<Badge />, document.getElementById('root'));
+const fotoAndy = "https://instagram.fmex7-1.fna.fbcdn.net/vp/1119985ce3409471f3631c73d30cdb0d/5E64C107/t51.2885-19/s150x150/49683380_796737684024181_8687158980700536832_n.jpg?_nc_ht=instagram.fmex7-1.fna.fbcdn.net";
+
+ReactDOM.render(
+    <Badge 
+        firstName="Andrew" 
+        lastName="merchant" 
+        jobTitle="Software developer"
+        twitter="andrerchant"
+        avatar={fotoAndy}
+    />, 
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
