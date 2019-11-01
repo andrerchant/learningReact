5e4c26e4212022b39fa64ca78986e522e5713321
andrerchant <andremerchant@gmail.com>
2019-10-31 18:21:40 -0600

    clases de la 33 a la 35 Modal y Hooks

--- src/pages/BadgeDetails.js
+++ src/pages/BadgeDetails.js
@@ -4,8 +4,20 @@ import { Link } from 'react-router-dom';
 import './styles/BadgeDetails.css';
 import confLogo from '../images/platziconf-logo.svg';
 import Badge from '../components/badge';
+import DeleteBadgeModal from '../components/DeleteBadgeModal';
+
+function useIncreaseCount(max){
+  const [ count, setCount ] = React.useState(0);
+
+  if(count > max){
+    setCount(0)
+  }
+
+  return [count,setCount];
+}
 
 function BadgeDetails(props) {
+  const [ count, setCount ] = useIncreaseCount(4);
   const badge = props.badge;
 
   return (
@@ -40,6 +52,13 @@ function BadgeDetails(props) {
             <h2>Actions</h2>
             <div>
               <div>
+
+                <button onClick={()=>{
+                  setCount(count +1)
+                }} className="btn btn-primary mr-4">
+                  Increase Count {count}
+                </button>
+
                 <Link
                   className="btn btn-primary mb-4"
                   to={`/badges/${badge.id}/edit`}
@@ -49,7 +68,13 @@ function BadgeDetails(props) {
               </div>
 
               <div>
-                <button className="btn btn-danger">Delete</button>
+              {/* onClick={this.openModal} */}
+                <button onClick={props.onOpenModal} className="btn btn-danger">Delete</button>
+                <DeleteBadgeModal 
+                  isOpen={props.modalIsOpen} 
+                  onClose={props.onCloseModal} 
+                  onDeleteBadge={props.onDeleteBadge}
+                />
               </div>
             </div>
           </div>
